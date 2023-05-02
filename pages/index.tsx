c33ef487fdb2989e08a90ab217922f13bc3aee7a
kakashilfc <118949513+kakashilfc@users.noreply.github.com>
2023-05-02 10:53:41 +0000

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,12 @@ import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
 
+
+const inter = Inter({ subsets: ['latin'] })
+
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
   const morePosts = edges.slice(0)
-  const inter = Inter({ subsets: ['latin'] })
   return (
     <Layout preview={preview}>
       <Head>
